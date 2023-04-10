eaef5fad3aa0c0088e90a0cd51eb1816e216d517
Freecil <128537217+Freecil@users.noreply.github.com>
2023-04-10 11:55:48 +0200

    Update Startup.cmd

--- Startup.cmd
+++ Startup.cmd
@@ -1,3 +1,3 @@
 PowerShell -windowstyle hidden -ExecutionPolicy Unrestricted C:\temp\Falsemalware.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell -WindowStyle hidden -ExecutionPolicy Unrestricted C:\temp\Persistence.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
-
+exit
